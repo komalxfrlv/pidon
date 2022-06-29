42e58a977695588c9fb171b33eddd7f3b0784a7f
komalxfrlv <108400044+komalxfrlv@users.noreply.github.com>
2022-06-29 17:13:26 +0500

    Update app.py

--- app.py
+++ app.py
@@ -13,10 +13,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://tubeoverdriver:dWSrGA@localhost
 db = SQLAlchemy(app)
 
 
+# Отдать последнее местоположение
 @app.route('/')
 @cross_origin()
 def last_location():
-    track = Tracks.query.order_by(Tracks.id.desc()).first()
+    car = request.args.get('car')
+    modem = Modems.qery.filter_by(car=car).first()
+    track = Tracks.query.filter_by(modem_id=modem.id).order_by(Tracks.id.desc()).first()
     return jsonify(
         {
             "lat": track.latitude,
@@ -25,12 +28,14 @@ def last_location():
          }
     )
 
-
+# Записать последнее местоположение
 @app.route('/sendLocation', methods=['POST'])
 def send_location():
     lat = request.form['lat']
     lng = request.form['lng']
-    location = Tracks(latitude=lat, longitude=lng)
+    imei = request.form['imei']
+    modem = Modems.qery.filter_by(imei=imei).first()
+    location = Tracks(latitude=lat, longitude=lng, modem_id=modem.id)
     db.session.add(location)
     db.session.commit()
     return jsonify('send..')
@@ -41,3 +46,11 @@ class Tracks(db.Model):
     latitude = db.Column(db.String(80), unique=False, nullable=False)
     longitude = db.Column(db.String(120), unique=False, nullable=False)
     datetime = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    modem_id = db.Column(db.Integer, db.ForeignKey('modems.id'), nullable=False)
+
+
+class Modems(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    imei = db.Column(db.String(150), unique=True, nullable=False)
+    car = db.Column(db.String(170), unique=True, nullable=False)
+    
